49ace827dd104ad8ad69302763568fb654adb920
fabianvanrissenbeck <fabian.vanrissenbeck@gmail.com>
2026-01-29 13:27:48 +0100

    allow skipping MRAM benchmark; more verbose output

--- run.sh
+++ run.sh
@@ -7,8 +7,11 @@ RUN_PRIM="no"
 # BFS and MLP benchmarks with multiple inputs
 RUN_BFSMLP="no"
 
-# Microbenchmarks except subkernel load times/key exchange/ready line
-RUN_MICRO="no"
+# Benchmark evaluating MRAM throughput
+RUN_MRAM="no"
+
+# Benchmark measuring sealed MRAM transfer times
+RUN_CRYPTO="no"
 
 # Microbenchmark reporting subkernel load times/key exchange and ready line
 RUN_SUBK="no"
@@ -21,6 +24,7 @@ function print_help_exit {
   echo "Run benchmarks within the memclave environment. Usually no command line option is required."
   echo "You may alter the number of benchmarks executed by passing one of the following parameters:"
   echo "all    - Run all benchmarks even those requiring a modified TL"
+  echo "fast   - Run default benchmarks except for the MRAM one"
   echo "prim   - Run the full PrIM benchmark suite with default input sizes"
   echo "bfsmlp - Run the MLP and BFS benchmarks with different input sizes"
   echo "micro  - Run the MRAM throughput and sealed EM transfer benchmarks"
@@ -33,15 +37,23 @@ if [ "$#" == "0" ];
 then
   RUN_PRIM="yes"
   RUN_BFSMLP="yes"
-  RUN_MICRO="yes"
+  RUN_MRAM="yes"
+  RUN_CRYPTO="yes"
 elif [ "$#" == "1" ];
 then
   if [ "$1" == "all" ];
   then
     RUN_PRIM="yes"
     RUN_BFSMLP="yes"
-    RUN_MICRO="yes" 
+    RUN_MRAM="yes"
+    RUN_CRYPTO="yes"
     RUN_SUBK="yes"
+  elif [ "$1" == "fast" ];
+  then
+    RUN_PRIM="yes"
+    RUN_BFSMLP="yes"
+    RUN_CRYPTO="yes"
+    SKIP_MRAM="yes"
   elif [ "$1" == "prim" ];
   then
     RUN_PRIM="yes"
@@ -50,7 +62,8 @@ then
     RUN_BFSMLP="yes"
   elif [ "$1" == "micro" ];
   then
-    RUN_MICRO="yes"
+    RUN_CRYPTO="yes"
+    RUN_MRAM="yes"
   elif [ "$1" == "subk" ];
   then
     RUN_SUBK="yes"
@@ -64,7 +77,8 @@ fi
 echo "=== Benchmark Configuration ==="
 echo "RUN_PRIM: $RUN_PRIM"
 echo "RUN_BFSMLP: $RUN_BFSMLP"
-echo "RUN_MICRO: $RUN_MICRO"
+echo "RUN_CRYPTO: $RUN_CRYPTO"
+echo "RUN_MRAM: $RUN_MRAM"
 echo "RUN_SUBK: $RUN_SUBK"
 
 echo "Writing outputs to: $OUTDIR"
@@ -90,6 +104,7 @@ then
   echo "=== Running PrIM Benchmarks ==="
   ./run_prim.sh all
   mv ./build/prim_results.csv $OUTDIR/
+  echo "Wrote results to $OUTDIR/prim_results.csv"
   echo ""
 fi
 
@@ -98,28 +113,35 @@ then
   echo "=== Running MLP Benchmarks ==="
   python3 run_mlp.py --mode memclave --cwd build
   mv mlp_results.csv $OUTDIR/
+  echo "Wrote results to $OUTDIR/mlp_results.csv"
   echo ""
 
   echo "=== Running BFS Benchmarks ==="
   python3 run_bfs.py --mode memclave --cwd ./build --graph-prefix ../data
   mv bfs_results.csv $OUTDIR/
+  echo "Wrote results to $OUTDIR/bfs_results.csv"
   echo ""
 fi
 
-if [ "$RUN_MICRO" == "yes" ];
+if [ "$RUN_MRAM" == "yes" ];
 then
   echo "=== Running MRAM Benchmark (This one takes ~30 minutes) ==="
   cd build
   ./mram > mram.csv
   cd ..
   mv build/mram.csv $OUTDIR/
+  echo "Wrote results to $OUTDIR/mram.csv"
   echo ""
+fi
 
+if [ "$RUN_CRYPTO" == "yes" ];
+then
   echo "=== Running Sealed EM Benchmark ==="
   cd build
   ./crypto-bench > crypto.csv
   cd ..
   mv build/crypto.csv $OUTDIR/
+  echo "Wrote results to $OUTDIR/crypto.csv"
   echo ""
 fi
 
@@ -129,6 +151,7 @@ then
   cd build
   ./sk-load-bench > sk.csv
   cd ..
-  mv build.sk.csv $OUTDIR/
+  mv build/sk.csv $OUTDIR/
+  echo "Wrote results to $OUTDIR/sk.csv"
   echo ""
 fi
